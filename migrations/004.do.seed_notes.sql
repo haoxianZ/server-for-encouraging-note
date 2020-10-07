e4f8fd7a9728d8c368f3d2ec28fa96d591bd78f4
John Zhang <johnhaoxian@gmail.com>
2020-10-07 09:22:52 -0400

    uuid fk

--- migrations/004.do.seed_notes.sql
+++ migrations/004.do.seed_notes.sql
@@ -1,6 +1,6 @@
 INSERT INTO notes (content, user_id)
 VALUES
-  ('Come on! You can do it!.','7db698d6-5074-4343-afcf-edc99305ca68'),
-  ('If you''re feeling helpless, help someone.','5779e1ed-47a0-4e96-be59-316aca83d0dc'),
-  ('Keep up the good work!','7db698d6-5074-4343-afcf-edc99305ca68'),
-  ('You''re not dead yet.','7db698d6-5074-4343-afcf-edc99305ca68');
+  ('Come on! You can do it!.','7db698d6-5074-4343-afcf-edc99305ca68'::uuid),
+  ('If you''re feeling helpless, help someone.','5779e1ed-47a0-4e96-be59-316aca83d0dc'::uuid),
+  ('Keep up the good work!','7db698d6-5074-4343-afcf-edc99305ca68'::uuid),
+  ('You''re not dead yet.','7db698d6-5074-4343-afcf-edc99305ca68'::uuid);
