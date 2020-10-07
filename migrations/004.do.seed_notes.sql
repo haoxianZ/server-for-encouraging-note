15d0aafe1ac50273f32c606aa60b710c38e41b63
John Zhang <johnhaoxian@gmail.com>
2020-10-07 09:49:31 -0400

    just trying

--- migrations/004.do.seed_notes.sql
+++ migrations/004.do.seed_notes.sql
@@ -1,6 +1,6 @@
 INSERT INTO notes (content, user_id)
 VALUES
-  ('Come on! You can do it!.','testa'),
-  ('If you''re feeling helpless, help someone.','testb'),
-  ('Keep up the good work!','testb'),
-  ('You''re not dead yet.','testa');
+  ('Come on! You can do it!.','{7db698d6-5074-4343-afcf-edc99305ca68}'),
+  ('If you''re feeling helpless, help someone.','{5779e1ed-47a0-4e96-be59-316aca83d0dc}'),
+  ('Keep up the good work!','{7db698d6-5074-4343-afcf-edc99305ca68}'),
+  ('You''re not dead yet.','{7db698d6-5074-4343-afcf-edc99305ca68}');
