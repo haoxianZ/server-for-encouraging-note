857d57f8b833c54ab63203f8309a205b22e1fc94
John Zhang <johnhaoxian@gmail.com>
2020-10-07 09:40:41 -0400

    unique username

--- migrations/001.do.create_users.sql
+++ migrations/001.do.create_users.sql
@@ -2,6 +2,6 @@ CREATE EXTENSION pgcrypto;
 
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    username TEXT NOT NULL,
+    username TEXT NOT NULL UNIQUE,
     email TEXT
 );
