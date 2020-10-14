a568466831fbeb5b5e7694600c3b10d1c76a0558
John Zhang <johnhaoxian@gmail.com>
2020-10-14 14:38:59 -0400

    deploy

--- src/config.js
+++ src/config.js
@@ -2,5 +2,5 @@ require('dotenv').config()
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DATABASE_URL ||'postgresql://dunder_mifflin@localhost/Encouragement'
+  DATABASE_URL: process.env.DATABASE_URL ||'postgres://yhzksdvxtvaagw:0359c3f00f14ebe5c73dce97b4ff0e4d5e6c138d87585feda599f2f6f4f0b3ef@ec2-54-160-18-230.compute-1.amazonaws.com:5432/d87ktv3fnv548u'
 }
